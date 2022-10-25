cd41bb7a3bd081ce66b9851c1d7a8815def14cc1
sanjeev <sanjeevsingh.77d@gmail.com>
2022-10-25 22:05:51 +0530

    Fibonacci Series

---
+++ Day6_problems/Day6_problems/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day6_problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day6_Problems num = new Day6_Problems();
+            num.Fibonacci_Series();
+            
+        }
+    }
+}
